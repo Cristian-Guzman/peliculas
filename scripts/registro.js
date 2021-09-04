8dde889c02ae6e816f576cf025a3d67ca0ed3a6b
Cristian Guzman <cristiangt1813@gmail.com>
2021-09-03 20:29:38 -0500

    Añadiendo registro e inicio

---
+++ scripts/registro.js
@@ -0,0 +1,55 @@
+const registro = document.querySelector('.formulario');
+registro.innerHTML = `<form class="form">
+<div class="contenedor">
+        <h2 style="text-align: center;">Formulario de registro</h2>
+    <div class="columnas">
+        <div class="columna">
+            <label for="nombre">Nombre:</label>
+            <input class="nombre" type="text" name="nombre">
+        </div>  
+        <div class="columna">
+            <label for="apellido">Apellido:</label>
+            <input class="apellido" type="text" name="apellido">
+        </div>
+        <div class="columna">
+            <label for="telefono">Telefono:</label>
+            <input class="telefono" style="margin-right: 1px;" type="tel" name="telefono">
+        </div>
+        <div class="columna">
+            <label for="direccion">Direccion:</label>
+            <input class="direccion" style="margin-right: 5px;" type="text" name="direccion">
+        </div>
+        <div style="display: flex; flex-wrap: wrap;" class="columna">
+            <label for="observaciones">Observaciones:</label>
+            <textarea class="observacion" name="observaciones"cols="30" rows="10"></textarea>
+        </div>
+    </div>
+        <div class="boton__submit"><input type="submit" value="Enviar"></div>
+</div>
+</form>`; 
+
+const casillaNombre = document.querySelector('.nombre');
+const casillaApellido = document.querySelector('.apellido');
+const casillaTelefono = document.querySelector('.telefono');
+const casillaDireccion = document.querySelector('.direccion');
+const casillaObservacion = document.querySelector('.observacion');
+const submit = document.querySelector('.form');
+
+// Añadiendo la opción de que la información del localStorage se guarde al enviar el documento.
+submit.addEventListener('submit', e =>{ 
+    let signUp = {
+        nombre : casillaNombre.value,
+        apellido : casillaApellido.value,
+        telefono : casillaTelefono.value,
+        direccion : casillaDireccion.value,
+        observacion : casillaObservacion.value
+    }
+    localStorage.setItem('signUp', JSON.stringify(signUp));
+    e.preventDefault();
+    dispData();
+})
+
+const dispData = () =>{
+    let res = JSON.parse(localStorage.getItem('signUp'));
+    console.table(res);
+}
---
+++ scripts/script.js
@@ -0,0 +1,113 @@
+const infor = [
+    {imagen : "images/captain.jpg", informacion : "Capitana marvel"},
+    {imagen : "images/daylight.jpg", informacion : "Daylight's end"},
+    {imagen : "images/drstrange.jpg", informacion : "Doctor Strange"},
+    {imagen : "images/logan.jpg", informacion : "Logan"},
+    {imagen : "images/chris.jpg", informacion : "Mision de Rescate"},
+    {imagen : "images/shang.jpg", informacion : "Shang-Chi"},
+    {imagen : "images/tomorrowwar.jpg", informacion : "The tomorrow war"}
+]
+
+
+let indice = 1;
+muestraSlides(indice);
+
+function avanzaSlide(n){
+    muestraSlides( indice+=n );
+}
+
+function posicionSlide(n){
+    muestraSlides(indice=n);
+}
+setInterval(function tiempo(){
+    muestraSlides(indice+=1)
+},4000);
+
+const fragment = document.createDocumentFragment();
+const barras = document.querySelector('.barras');
+const material = document.querySelector('.nombrePelicula');
+
+infor.forEach(e => {
+    //Añadiendo la imagen de las peliculas
+    const span = document.createElement('SPAN');
+    const image = document.createElement('IMG');
+    const nombre = document.createElement('H4');
+    const link = document.createElement('A');
+    //Creando btn informacion
+    const botonInfo = document.createElement('BUTTON');
+    const botonInfoDiv = document.createElement('DIV');
+    const botonInfoImg = document.createElement('DIV');
+    const botonInfoSpan = document.createElement('SPAN');
+    //Creando btn Play
+    const botonPlay = document.createElement('BUTTON');
+    const botonPlayDiv = document.createElement('DIV');
+    const botonPlayImg = document.createElement('DIV');
+    const botonPlaySpan = document.createElement('SPAN');
+    //Creando un div para cada pelicula
+    const peliculas = document.createElement('DIV');
+
+    const botones = document.createElement('DIV');
+
+    botonPlaySpan.textContent = "Play";
+    botonPlayImg.innerHTML = `<svg viewBox="0 0 24 24"><path d="M6 4l15 8-15 8z" fill="currentColor"></path></svg>`
+    botonPlay.classList.add('botonPlay');
+    botonPlay.setAttribute("type", "button");
+
+    botonInfoSpan.textContent = "Más información"
+    botonInfoImg.innerHTML = `<svg viewBox="0 0 24 24"><path d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2s10 4.477 10 10zm-2 0a8 8 0 0 0-8-8 8 8 0 0 0-8 8 8 8 0 0 0 8 8 8 8 0 0 0 8-8zm-9 6v-7h2v7h-2zm1-8.75a1.21 1.21 0 0 1-.877-.364A1.188 1.188 0 0 1 10.75 8c0-.348.123-.644.372-.886.247-.242.54-.364.878-.364.337 0 .63.122.877.364.248.242.373.538.373.886s-.124.644-.373.886A1.21 1.21 0 0 1 12 9.25z" fill="currentColor"></path></svg>`;
+    botonInfo.classList.add('botonInfo');
+    botonInfo.setAttribute("type", "button");
+
+    botones.classList.add('botones');
+    
+    link.setAttribute("href", "#");
+    link.addEventListener("mouseover", (e)=> {
+        if (e) {
+            link.classList.add('link')
+        } else {
+            link.classList.remove('link');
+        }
+    })
+    setInterval(function() {link.classList.remove('link'); }, 25000);
+    
+    botonPlay.appendChild(botonPlayDiv);
+    botonPlay.appendChild(botonPlaySpan);
+    botonPlayDiv.appendChild(botonPlayImg);
+    link.appendChild(image);
+    link.appendChild(nombre);
+    botonInfo.appendChild(botonInfoDiv);
+    botonInfoDiv.appendChild(botonInfoImg);
+    botonInfoDiv.appendChild(botonInfoSpan);
+    span.appendChild(link);
+    botones.appendChild(botonPlay);
+    botones.appendChild(botonInfo);
+    nombre.classList.add('nombrePelicula');
+    nombre.textContent = e.informacion;
+    image.setAttribute("src", `${e.imagen}`);
+    span.classList.add('barra');
+    peliculas.appendChild(span);
+    peliculas.appendChild(botones);
+    fragment.appendChild(peliculas);
+    // span.innerHTML = e.informacion;
+});
+barras.appendChild(fragment);
+
+function muestraSlides(n){
+    let i;
+    let slides = document.querySelectorAll('.miSlider');
+    // let barras = document.querySelectorAll('.barra');
+
+    if(n > slides.length){
+        indice = 1;
+    }
+    if(n < 1){
+        indice = slides.length;
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    slides[indice-1].style.display = 'block';
+}
+
+
+
